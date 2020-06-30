419d32dd8c6cf011b48ab71be7c5557847d7db86
Wojciech Trojanowski <W.Trojan27@gmail.com>
2020-06-30 19:18:47 +0200

    Add: Refactore to OOP

--- cl300.py
+++ cl300.py
@@ -7,33 +7,43 @@ path_out = "output"
 nsCommon = "{http://www.joia.or.jp/standardized/namespaces/Common}"
 nsLM = "{http://www.joia.or.jp/standardized/namespaces/LM}"
 
-def getObj (path, ns):
-    return root.find(path.replace("/", "/{0}").format(ns))
+class Measure:
+    def __init__ (self, file, path_in="input", path_out="output"):
+        self.file = file
+        tree = ET.parse(join(path_in, file))
+        self.root = tree.getroot()
+
+    def getObj (self, path, ns):
+        return self.root.find(path.replace("/", "/{0}").format(ns))
+
+    def getDetails(self):
+        self.details = {
+            'date': self.getObj("./Common/Date", nsCommon).text,
+            'time' : self.getObj("./Common/Time", nsCommon).text,
+            'measuer_id' : self.getObj("./Common/Patient/ID", nsCommon).text,
+
+            'lensType' : self.getObj("./Measure/LensType", nsLM).text,
+
+            'rSphere' : self.getObj("./Measure/LM/R/Sphere", nsLM).text,
+            'rCylinder' : self.getObj("./Measure/LM/R/Cylinder", nsLM).text,
+            'rAxis' : self.getObj("./Measure/LM/R/Axis", nsLM).text,
+            'rAdd1Unit' : self.getObj("./Measure/LM/R/Add1", nsLM).attrib['unit'],
+            'rAdd2Unit' : self.getObj("./Measure/LM/R/Add2", nsLM).attrib['unit'],
+            'rHPrism' : self.getObj("./Measure/LM/R/H", nsLM).attrib['Prism'],
+            'rVPrism' : self.getObj("./Measure/LM/R/V", nsLM).attrib['Prism'],
+
+            'lSphere' : self.getObj("./Measure/LM/L/Sphere", nsLM).text,
+            'lCylinder' : self.getObj("./Measure/LM/L/Cylinder", nsLM).text,
+            'lAxis' : self.getObj("./Measure/LM/L/Axis", nsLM).text,
+            'lrAdd1Unit' : self.getObj("./Measure/LM/L/Add1", nsLM).attrib['unit'],
+            'lAdd2Unit' : self.getObj("./Measure/LM/L/Add2", nsLM).attrib['unit'],
+            'lHPrism' : self.getObj("./Measure/LM/L/H", nsLM).attrib['Prism'],
+            'lVPrism' : self.getObj("./Measure/LM/L/V", nsLM).attrib['Prism']
+        }
 
 files = [file for file in listdir(path_in) if isfile(join(path_in , file))]
 
 for file in files:
-    tree = ET.parse(join(path_in, file))
-    root = tree.getroot()
-
-    date = getObj("./Common/Date", nsCommon).text
-    time = getObj("./Common/Time", nsCommon).text
-    measuer_id = getObj("./Common/Patient/ID", nsCommon).text
-
-    lensType = getObj("./Measure/LensType", nsLM).text
-
-    rSphere = getObj("./Measure/LM/R/Sphere", nsLM).text
-    rCylinder = getObj("./Measure/LM/R/Cylinder", nsLM).text
-    rAxis = getObj("./Measure/LM/R/Axis", nsLM).text
-    rAdd1Unit = getObj("./Measure/LM/R/Add1", nsLM).attrib['unit']
-    rAdd2Unit = getObj("./Measure/LM/R/Add2", nsLM).attrib['unit']
-    rHPrism = getObj("./Measure/LM/R/H", nsLM).attrib['Prism']
-    rVPrism = getObj("./Measure/LM/R/V", nsLM).attrib['Prism']
-
-    lSphere = getObj("./Measure/LM/L/Sphere", nsLM).text
-    lCylinder = getObj("./Measure/LM/L/Cylinder", nsLM).text
-    lAxis = getObj("./Measure/LM/L/Axis", nsLM).text
-    lrAdd1Unit = getObj("./Measure/LM/L/Add1", nsLM).attrib['unit']
-    lAdd2Unit = getObj("./Measure/LM/L/Add2", nsLM).attrib['unit']
-    lHPrism = getObj("./Measure/LM/L/H", nsLM).attrib['Prism']
-    lVPrism = getObj("./Measure/LM/L/V", nsLM).attrib['Prism']
+    measure = Measure(file)
+    measure.getDetails()
+    print(measure.details)
